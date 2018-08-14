0ac02fe1114ded72fbe53eae9486fec578714b6a
rodneystrong <akang2@gmail.com>
2018-08-13 21:19:17 -0700

    destructuring example and logging it

--- docs/alex/5/work/input.mjs
+++ docs/alex/5/work/input.mjs
@@ -5,3 +5,12 @@ let number = process.argv[2];
 let result = add(number >> 0, 2);
 
 console.log(result);
+
+let post = {
+  title: 'test',
+  desc: 1,
+};
+
+let { title, desc } = post;
+
+console.log(`the post desc is ${desc}`);
